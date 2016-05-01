1456f83be6e13ba22ab785f15c557700e4ed1b4f
Axel Monroy <xaxelmonroyx@gmail.com>
2016-05-01 15:33:57 -0700

    Agregado un ejercicio de completar palabras, verificandolo y con voz

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,22 @@
 angular.module('app.controllers', [])
 
-<<<<<<< HEAD
-.controller('homeCtrl', function($scope) {
-=======
+
 .controller('homeCtrl', function($scope, $http) {
 
-	$http.get("js/user.json")
-		.then(function(response) {
-			$scope.myWelcome = response.data.nombre;
-			if (response.data.nombre!="") {$scope.speakText('Bienvenido ' + response.data.nombre);};
-			//$scope.speakText('Bienvenido ' + response.data.nombre);
-		});
-	$scope.speakText = function(text) {
-		var speech = new SpeechSynthesisUtterance(text);
-		speech.lang = 'es-ES';
-		speechSynthesis.speak(speech);
-	};
->>>>>>> completarpalabras
+    $http.get("js/user.json")
+        .then(function(response) {
+            $scope.myWelcome = response.data.nombre;
+            if (response.data.nombre != "") {
+                $scope.speakText('Bienvenido ' + response.data.nombre);
+            };
+            //$scope.speakText('Bienvenido ' + response.data.nombre);
+        });
+    $scope.speakText = function(text) {
+        var speech = new SpeechSynthesisUtterance(text);
+        speech.lang = 'es-ES';
+        speechSynthesis.speak(speech);
+    };
+
 
 })
 
@@ -37,28 +37,60 @@ angular.module('app.controllers', [])
 })
 
 .controller('felicidadesCtrl', function($scope) {
+    $scope.speakText = function(text) {
+        var speech = new SpeechSynthesisUtterance(text);
+        speech.lang = 'es-ES';
+        speechSynthesis.speak(speech);
+    };
+
+    $scope.speakText('Eres genial, has ganado 10 monedas');
+
 
 })
 
 .controller('vuelveAIntentarloCtrl', function($scope) {
+    $scope.speakText = function(text) {
+        var speech = new SpeechSynthesisUtterance(text);
+        speech.lang = 'es-ES';
+        speechSynthesis.speak(speech);
+    };
+
+    $scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
 
 })
 
-.controller('completarPalabras1Ctrl', function($scope) {
-  $scope.items=['al','el','il','ol','ul'];
+.controller('completarPalabras1Ctrl', function($scope, $state) {
+    var respuestaActual;
+    var respuestaCorrecta = 'ol';
+    $scope.items = ['al', 'el', 'il', 'ol', 'ul'];
 
-<<<<<<< HEAD
+
+    $scope.speakText = function(text) {
+        var speech = new SpeechSynthesisUtterance(text);
+        speech.lang = 'es-ES';
+        speechSynthesis.speak(speech);
+    };
+
+    $scope.speakText('Selecciona solo una opcion');
+
+
+
+    $scope.respuesta = function(text) {
+        respuestaActual = text;
+    };
+    $scope.verificar = function() {
+        if (respuestaActual == respuestaCorrecta) {
+            $state.go('menu.felicidades', {}, {
+                reload: true
+            });
+        } else {
+        	//$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
+            $state.go('vuelveAIntentarlo', {}, {
+                reload: true
+            });
+        }
+    };
+
+
+
 })
-=======
-
-
-	$scope.speakText = function(text) {
-		var speech = new SpeechSynthesisUtterance(text);
-		speech.lang = 'es-ES';
-		speechSynthesis.speak(speech);
-	};
-
-	$scope.speakText('Selecciona solo una opcion');
-
-})
->>>>>>> completarpalabras
