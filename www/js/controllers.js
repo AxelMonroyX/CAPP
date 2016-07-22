3c06cdef15d826b43dd754a521513e796110bf1f
Axel Monroy <xaxelmonroyx@gmail.com>
2016-07-22 13:08:09 -0700

    Creado el service SpeakService para no repetir la funcion siguiendo DRY

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,437 +1,234 @@
 angular.module('app.controllers', ['ionic'])
 
 
-.controller('homeCtrl', function($scope, $http) {
+.controller('homeCtrl', function ($scope, $http, SpeakService) {
 
-    $http.get("js/user.json")
-        .then(function(response) {
-            $scope.myWelcome = response.data.nombre;
-            if (response.data.nombre != "") {
-                $scope.speakText('Bienvenido ' + response.data.nombre);
-            };
-            //$scope.speakText('Bienvenido ' + response.data.nombre);
-        });
-
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
-
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
-
-
-
-    };
+  $http.get("js/user.json")
+    .then(function (response) {
+      $scope.myWelcome = response.data.nombre;
+      if (response.data.nombre != "") {
+        SpeakService.speakText('Bienvenido ' + response.data.nombre);
+      };
+      //$scope.speakText('Bienvenido ' + response.data.nombre);
+    });
 
 
 })
 
-.controller('cartCtrl', function($scope) {
+.controller('cartCtrl', function ($scope) {
 
 })
 
-.controller('cloudCtrl', function($scope) {
+.controller('cloudCtrl', function ($scope) {
 
 })
 
-.controller('tiposDeJuegosCtrl', function($scope) {
+.controller('tiposDeJuegosCtrl', function ($scope) {
 
 })
 
-.controller('completarPalabrasCtrl', function($scope) {
+.controller('completarPalabrasCtrl', function ($scope) {
 
 })
 
-.controller('felicidadesCtrl', function($scope) {
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
-
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
+.controller('felicidadesCtrl', function ($scope, SpeakService) {
+  $scope.speakText = function (text) {
+    SpeakService.speakText(text);
+  };
 
 
-
-    };
-
-
-    $scope.speakText('Eres genial, has ganado 10 monedas');
+  $scope.speakText('Eres genial, has ganado 10 monedas');
 
 
 })
 
-.controller('vuelveAIntentarloCtrl', function($scope) {
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
-
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
+.controller('vuelveAIntentarloCtrl', function ($scope, SpeakService) {
+  $scope.speakText = function (text) {
+    SpeakService.speakText(text);
 
 
+  };
 
-    };
-
-    $scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
+  $scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
 
 })
 
-.controller('completarPalabras1Ctrl', function($scope, $state) {
-    var respuestaActual;
-    var respuestaCorrecta = 'ol';
-    $scope.items = ['al', 'el', 'il', 'ol', 'ul'];
+.controller('completarPalabras1Ctrl', function ($scope, $state, SpeakService) {
+  var respuestaActual;
+  var respuestaCorrecta = 'ol';
+  $scope.items = ['al', 'el', 'il', 'ol', 'ul'];
 
 
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
+  $scope.speakText = function (text) {
+    SpeakService.speakText(text)
 
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
+
+  };
+
+  $scope.speakText('Selecciona solo una opcion');
 
 
 
-    };
-
-    $scope.speakText('Selecciona solo una opcion');
-
-
-
-    $scope.respuesta = function(text) {
-        respuestaActual = text;
-    };
-    $scope.verificar = function() {
-        if (respuestaActual == respuestaCorrecta) {
-            $state.go('menu.felicidades', {}, {
-                reload: true
-            });
-        } else {
-            //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
-            $state.go('vuelveAIntentarlo', {}, {
-                reload: true
-            });
-        }
-    };
+  $scope.respuesta = function (text) {
+    respuestaActual = text;
+  };
+  $scope.verificar = function () {
+    if (respuestaActual == respuestaCorrecta) {
+      $state.go('menu.felicidades', {}, {
+        reload: true
+      });
+    } else {
+      //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
+      $state.go('vuelveAIntentarlo', {}, {
+        reload: true
+      });
+    }
+  };
 
 
 
 })
 
 
-.controller('completarPalabras1Ctrl2', function($scope, $state) {
-    var respuestaActual;
-    var respuestaCorrecta = 'co';
-    $scope.items = ['te', 'ba', 'co', 'ci', 'ce'];
+.controller('completarPalabras1Ctrl2', function ($scope, $state, SpeakService) {
+  var respuestaActual;
+  var respuestaCorrecta = 'co';
+  $scope.items = ['te', 'ba', 'co', 'ci', 'ce'];
 
 
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
+  $scope.speakText = function (text) {
+    SpeakService.speakText(text);
 
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
+
+  };
+
+  $scope.speakText('Selecciona solo una opcion');
 
 
 
-    };
-
-    $scope.speakText('Selecciona solo una opcion');
-
-
-
-    $scope.respuesta = function(text) {
-        respuestaActual = text;
-    };
-    $scope.verificar = function() {
-        if (respuestaActual == respuestaCorrecta) {
-            $state.go('menu.felicidades', {}, {
-                reload: true
-            });
-        } else {
-            //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
-            $state.go('vuelveAIntentarlo', {}, {
-                reload: true
-            });
-        }
-    };
+  $scope.respuesta = function (text) {
+    respuestaActual = text;
+  };
+  $scope.verificar = function () {
+    if (respuestaActual == respuestaCorrecta) {
+      $state.go('menu.felicidades', {}, {
+        reload: true
+      });
+    } else {
+      //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
+      $state.go('vuelveAIntentarlo', {}, {
+        reload: true
+      });
+    }
+  };
 
 
 
 })
 
 
-.controller('completarPalabras1Ctrl3', function($scope, $state) {
-    var respuestaActual;
-    var respuestaCorrecta = 'cera';
-    $scope.items = ['mesa', 'sa', 'ra', 'cera', 'nace'];
+.controller('completarPalabras1Ctrl3', function ($scope, $state, SpeakService) {
+  var respuestaActual;
+  var respuestaCorrecta = 'cera';
+  $scope.items = ['mesa', 'sa', 'ra', 'cera', 'nace'];
 
 
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
+  $scope.speakText = function (text) {
+    SpeakService.speakText(text)
 
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
+  };
+
+  $scope.speakText('Selecciona solo una opcion');
 
 
 
-    };
-
-    $scope.speakText('Selecciona solo una opcion');
-
-
-
-    $scope.respuesta = function(text) {
-        respuestaActual = text;
-    };
-    $scope.verificar = function() {
-        if (respuestaActual == respuestaCorrecta) {
-            $state.go('menu.felicidades', {}, {
-                reload: true
-            });
-        } else {
-            //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
-            $state.go('vuelveAIntentarlo', {}, {
-                reload: true
-            });
-        }
-    };
+  $scope.respuesta = function (text) {
+    respuestaActual = text;
+  };
+  $scope.verificar = function () {
+    if (respuestaActual == respuestaCorrecta) {
+      $state.go('menu.felicidades', {}, {
+        reload: true
+      });
+    } else {
+      //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
+      $state.go('vuelveAIntentarlo', {}, {
+        reload: true
+      });
+    }
+  };
 
 
 
 })
 
 
-.controller('completarPalabras1Ctrl4', function($scope, $state) {
-    var respuestaActual;
-    var respuestaCorrecta = 'tón';
-    $scope.items = ['tan', 'tón', 'pón', 'zón', 'tin'];
+.controller('completarPalabras1Ctrl4', function ($scope, $state, SpeakService) {
+  var respuestaActual;
+  var respuestaCorrecta = 'tón';
+  $scope.items = ['tan', 'tón', 'pón', 'zón', 'tin'];
 
 
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
+  $scope.speakText = function (text) {
+    SpeakService.speakText(text)
 
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
+  };
+
+  $scope.speakText('Selecciona solo una opcion');
 
 
 
-    };
-
-    $scope.speakText('Selecciona solo una opcion');
-
-
-
-    $scope.respuesta = function(text) {
-        respuestaActual = text;
-    };
-    $scope.verificar = function() {
-        if (respuestaActual == respuestaCorrecta) {
-            $state.go('menu.felicidades', {}, {
-                reload: true
-            });
-        } else {
-            //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
-            $state.go('vuelveAIntentarlo', {}, {
-                reload: true
-            });
-        }
-    };
+  $scope.respuesta = function (text) {
+    respuestaActual = text;
+  };
+  $scope.verificar = function () {
+    if (respuestaActual == respuestaCorrecta) {
+      $state.go('menu.felicidades', {}, {
+        reload: true
+      });
+    } else {
+      //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
+      $state.go('vuelveAIntentarlo', {}, {
+        reload: true
+      });
+    }
+  };
 
 
 
 })
 
 
-.controller('completarPalabras1Ctrl5', function($scope, $state) {
-    var respuestaActual;
-    var respuestaCorrecta = 'Ca';
-    $scope.items = ['Ta', 'Ca', 'Pa', 'To', 'Da'];
+.controller('completarPalabras1Ctrl5', function ($scope, $state, SpeakService) {
+  var respuestaActual;
+  var respuestaCorrecta = 'Ca';
+  $scope.items = ['Ta', 'Ca', 'Pa', 'To', 'Da'];
 
 
-    $scope.speakText = function(text) {
-        //Web
-        try {
-            var speech = new SpeechSynthesisUtterance(text);
-            speech.lang = 'es-ES';
-            speechSynthesis.speak(speech);
-        } catch (e) {
+  $scope.speakText = function (text) {
+    SpeakService.speakText(text)
 
-            console.log(e);
-        }
-        //Android
-        try {
-            TTS.speak({
-                text: text,
-                locale: 'es-ES',
-                rate: 1.5
-            }, function() {
-                // Do Something after success
-            }, function(reason) {
-                // Handle the error case
-            });
-        } catch (e) {
-            // statements
-            console.log(e);
-        }
+  };
+
+  $scope.speakText('Selecciona solo una opcion');
 
 
 
-    };
-
-    $scope.speakText('Selecciona solo una opcion');
-
-
-
-    $scope.respuesta = function(text) {
-        respuestaActual = text;
-    };
-    $scope.verificar = function() {
-        if (respuestaActual == respuestaCorrecta) {
-            $state.go('menu.felicidades', {}, {
-                reload: true
-            });
-        } else {
-            //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
-            $state.go('vuelveAIntentarlo', {}, {
-                reload: true
-            });
-        }
-    };
+  $scope.respuesta = function (text) {
+    respuestaActual = text;
+  };
+  $scope.verificar = function () {
+    if (respuestaActual == respuestaCorrecta) {
+      $state.go('menu.felicidades', {}, {
+        reload: true
+      });
+    } else {
+      //$scope.speakText('Puedes hacerlo mejor, pero toma 2 monedas');
+      $state.go('vuelveAIntentarlo', {}, {
+        reload: true
+      });
+    }
+  };
 
 
 
@@ -440,14 +237,14 @@ angular.module('app.controllers', ['ionic'])
 
 
 
-.controller('acercaDeCtrl', function($scope) {
+.controller('acercaDeCtrl', function ($scope) {
 
 })
 
-.controller('instruccionesCtrl', function($scope) {
+.controller('instruccionesCtrl', function ($scope) {
 
 })
 
-.controller('usuarioCtrl', function($scope) {
+.controller('usuarioCtrl', function ($scope) {
 
 })
--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,37 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('BlankFactory', [function () {
 
 }])
 
-.service('BlankService', [function(){
+.service('SpeakService', [function () {
+  this.speakText = function (text) {
+    //Web
+    try {
+      var speech = new SpeechSynthesisUtterance(text);
+      speech.lang = 'es-ES';
+      speechSynthesis.speak(speech);
+    } catch (e) {
 
+      console.log(e);
+    }
+    //Android
+    try {
+      TTS.speak({
+        text: text,
+        locale: 'es-ES',
+        rate: 1.5
+      }, function () {
+        // Do Something after success
+      }, function (reason) {
+        // Handle the error case
+      });
+    } catch (e) {
+      // statements
+      console.log(e);
+    }
+
+
+
+  };
 }]);
-
